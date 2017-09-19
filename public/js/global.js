f9d1c596ee7aec5986b40b57b83bd0abf57e7618
Paul Barrick <paul@imperativedesign.net>
2017-09-19 14:50:32 -0400

    scrolling behaviour

--- public/js/global.js
+++ public/js/global.js
@@ -1,9 +1,72 @@
 
 (function($){
-    $('document').ready(function($){
-        $('.folio').slick({
-            infinite: true,
-            slidesToShow: 4
-          });
-    });
+    $('document').ready(function($){ init() });
 })(jQuery)
+
+
+function init(){
+    bootstrapSlider();
+    bindEvents();
+}
+
+function bindEvents(){
+    $('.nav li a').on('click', scrollToElem);
+    $('.btt').on('click', scrollToTop);
+
+    //only run this on desktop
+    $(window).scroll(() => { 
+        fixElemToContainer('.home-sidebar form', '.testimonials'); 
+    }).trigger('scroll');
+}
+
+function fixElemToContainer(sel, relativeToSel){
+    var $elem       = $(sel);
+    var offset      = $(relativeToSel).offset().top - ($('.header-wrapper').outerHeight() + 15);   //account for fixed header
+    var scrollPos   = $(window).scrollTop() - offset;
+    var toTop       = $(window).scrollTop();
+    
+        
+    if(toTop >= offset){ 
+        window.setTimeout(() => {
+            $elem.animate({
+                'top': scrollPos
+            }, 25); 
+        }, 300);
+    }
+}
+
+function bootstrapSlider(){
+    $('.folio').slick({
+        infinite: true,
+        slidesToShow: 4
+      });
+}
+
+function scrollToElem(evt){
+    evt.preventDefault();
+    $(this).parent().parent().find('li a.active').removeClass('active');
+    $(this).addClass('active');
+
+    var $elem = $(evt.target.getAttribute('href'));
+    $(window).scrollTo($elem, 800, {
+        onAfter: showBttBtn,
+        offset: -145
+    });
+}
+
+function scrollToTop(evt){
+    evt.preventDefault();
+    $('.nav li a.active').removeClass('active');
+    
+    $(window).scrollTo(0, 800, {
+        onAfter: hideBttBtn
+    });
+}
+
+function showBttBtn(target, settings){
+    $('.btt').fadeIn(250);
+}
+
+function hideBttBtn(){
+    $('.btt').fadeOut(250);
+}
